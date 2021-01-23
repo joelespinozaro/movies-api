fdb8495dbbe869b9475e0200abf0e293159044b3
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-01-23 01:44:55 -0500

    middleware: manejo de errores

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const app = express();
 const { config } = require('./config');
 const moviesAPI = require('./routes/movies.js');
 
+const {
+  logErrors,
+  errorHandler,
+} = require('./utils/middleware/errorHandlers.js');
+
 //body parser middleware
 app.use(express.json());
 
 moviesAPI(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
