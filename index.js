c8065382b3dece6fdd5b764c5d3c60cd19cd78b1
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-01-23 01:22:27 -0500

    connection mongo atlas, services modified

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const { config } = require('./config');
 const moviesAPI = require('./routes/movies.js');
 
+//body parser middleware
+app.use(express.json());
+
 moviesAPI(app);
 
 app.listen(config.port, function () {
